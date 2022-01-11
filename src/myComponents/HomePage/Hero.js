ea815fb5360a0c2a2a3b6d6e48e51d702828c038
Ahsan Shahid <ahsanshahid2882@gmail.com>
2022-01-11 13:51:50 +0500

    Update Hero.js

--- src/myComponents/HomePage/Hero.js
+++ src/myComponents/HomePage/Hero.js
@@ -1,10 +1,22 @@
-import React from 'react'
-// import Header from './Header'
+import React from 'react';
+import Header from './Header';
+import HeroCss from './Hero.module.css';
+import heroImg from '../../assets/img/heroImg.jpg'
 
 export default function Hero() {
     return (
         <>
-
+            <Header />
+            <section className={`${HeroCss.container1} flex items-center justify-between my-20`}>
+                <section className="leftSec w-7/12">
+                    <h1 className='text-7xl font-bold tracking-widest text-indigo-700'>TASKER</h1>
+                    <p className='text-2xl tracking-tighter leading-10'>Organize your tasks the way you want!</p>
+                    <p className='text-xl font-light mt-8'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugiat porro praesentium omnis molestias vero sunt assumenda animi accusantium modi facilis possimus dolores, magnam cum, corrupti deserunt molestiae eos, ratione dolorem?</p>
+                </section>
+                <section className="rightSec">
+                    <img src={heroImg} alt="list of todos" className='h-72 object-cover'/>
+                </section>
+            </section>
         </>
     )
 }
