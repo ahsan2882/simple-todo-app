fb015ac13e2df88f49494c3bb4c49e3c249aad2d
Ahsan Shahid <ahsanshahid2882@gmail.com>
2022-01-11 19:05:07 +0500

    added new routes for other links

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Hero from './myComponents/HomePage/Hero';
+import { BrowserRouter as BRouter, Route, Routes } from 'react-router-dom';
+import About from './myComponents/AboutPage/About';
+import Home from './myComponents/HomePage/Home';
+import Login from './myComponents/LoginPage/Login';
+import Signup from './myComponents/RegisterPage/Signup';
+import Todo from './myComponents/TodoPage/Todo';
 
 function App() {
   return (
     <>
-      <Router>
+      <BRouter>
         <Routes>
-          <Route path="/" element={<Hero/>}/>
+          <Route path="/" element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Signup />} />
+          <Route path='/tasks' element={<Todo />} />
         </Routes>
-      </Router>
+      </BRouter>
     </>
   );
 }
