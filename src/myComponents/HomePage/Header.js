f143b88bff71197d133b6016d6105c2f766d5b44
Ahsan Shahid <ahsanshahid2882@gmail.com>
2022-01-11 19:05:31 +0500

    added tasks link in nav

--- src/myComponents/HomePage/Header.js
+++ src/myComponents/HomePage/Header.js
@@ -6,7 +6,7 @@ export default function Header() {
     return (
         <>
             <nav className="mainNav flex items-center justify-between">
-                <ul className="navbar flex items-stretch justify-around w-96 ml-8">
+                <ul className="navbar flex items-stretch justify-around ml-8">
                     <li className="navItem hover:shadow-lg flex items-center">
                         <Link to='/'>
                             <img src={logo} alt="logo" className="h-14" />
@@ -14,6 +14,9 @@ export default function Header() {
                     </li>
                     <li className="navItem hover:shadow-lg flex items-center">
                         <Link to='/about' className="p-6">About</Link>
+                    </li>
+                    <li className="navItem hidden items-center hover:shadow-lg">
+                        <Link to='/tasks' className="p-6">Tasks</Link>
                     </li>
                     <li className="navItem hover:shadow-lg flex items-center">
                         <Link to='/login' className="p-6">Login</Link>
