97075b4a194001be5167116063a970a69128b419
Ahsan Shahid <ahsanshahid2882@gmail.com>
2022-01-11 13:51:27 +0500

    fixed code structure and semantics

--- src/myComponents/HomePage/Header.js
+++ src/myComponents/HomePage/Header.js
@@ -1,14 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import logo from '../../assets/img/logo.jpg'
 
 export default function Header() {
     return (
         <>
-            <nav className="mainNav">
-                <ul className="navbar">
+            <nav className="mainNav flex items-center justify-between">
+                <ul className="navbar flex items-center justify-around w-96 ml-8">
                     <li className="navItem">
-                        <img src="" alt="logo" />
+                        <Link to='/'>
+                            <img src={logo} alt="logo" className="h-16" />
+                        </Link>
                     </li>
-                    <li className="navItem"></li>
+                    <li className="navItem">
+                        <Link to='/about'>About</Link>
+                    </li>
+                    <li className="navItem">
+                        <Link to='/login'>Login</Link>
+                    </li>
+                    <li className="navItem">
+                        <Link to='/register'>Signup</Link>
+                    </li>
+                </ul>
+                <ul className="rightNav flex">
                     <li className="navItem"></li>
                     <li className="navItem"></li>
                 </ul>
