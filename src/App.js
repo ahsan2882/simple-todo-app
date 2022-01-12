e6d02518c4576f85da4c784198962de1601d1911
Ahsan Shahid <ahsanshahid2882@gmail.com>
2022-01-12 12:20:42 +0500

    added context hook

--- src/App.js
+++ src/App.js
@@ -4,21 +4,30 @@ import Home from './myComponents/HomePage/Home';
 import Login from './myComponents/LoginPage/Login';
 import Signup from './myComponents/RegisterPage/Signup';
 import Todo from './myComponents/TodoPage/Todo';
-
+import React, { useState, createContext } from 'react';
+const AuthState = createContext();
+const setAuthState = createContext();
 function App() {
+  const [loginState, setloginState] = useState(false);
+  
   return (
     <>
-      <BRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/register' element={<Signup />} />
-          <Route path='/tasks' element={<Todo />} />
-        </Routes>
-      </BRouter>
+      <AuthState.Provider value={loginState}>
+        <setAuthState.Provider value={setloginState}>
+          <BRouter>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path='/about' element={<About />} />
+              <Route path='/login' element={<Login />} />
+              <Route path='/register' element={<Signup />} />
+              <Route path='/tasks' element={<Todo />} />
+            </Routes>
+          </BRouter>
+        </setAuthState.Provider>
+      </AuthState.Provider>
     </>
   );
 }
 
 export default App;
+export { AuthState, setAuthState };
