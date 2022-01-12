4e8876d5158c0d05ff8b72625d435c1a547c433b
Ahsan Shahid <ahsanshahid2882@gmail.com>
2022-01-12 19:11:17 +0500

    Create TodoItems.js

---
+++ src/myComponents/TodoPage/TodoItems.js
@@ -0,0 +1,39 @@
+let items = [
+    {
+        "title": "Todo Item 1",
+        "time": "today",
+        "urgency": "low",
+        "starred": false,
+        "status": "In Progress"
+    },
+    {
+        "title": "Todo Item 2",
+        "time": "tomorrow",
+        "urgency": "high",
+        "starred": true,
+        "status": "Done"
+    },
+    {
+        "title": "Todo Item 3",
+        "time": "tomorrow",
+        "urgency": "low",
+        "starred": false,
+        "status": "In Progress"
+    },
+    {
+        "title": "Todo Item 4",
+        "time": "Thursday",
+        "urgency": "medium",
+        "starred": true,
+        "status": "In Progress"
+    },
+    {
+        "title": "Todo Item 5",
+        "time": "Saturday",
+        "urgency": "high",
+        "starred": false,
+        "status": "Done"
+    }
+]
+
+export default items;
