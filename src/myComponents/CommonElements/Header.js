9184deba0c6e1ae1a823f3ceb342cde0c1eb8a8d
Ahsan Shahid <ahsanshahid2882@gmail.com>
2022-01-14 11:12:33 +0500

    Update Header.js

--- src/myComponents/CommonElements/Header.js
+++ src/myComponents/CommonElements/Header.js
@@ -8,23 +8,23 @@ export default function Header() {
     const auth = useContext(AuthState);
     return (
         <>
-            <nav className="mainNav flex items-center justify-between">
-                <ul className="navbar flex items-stretch justify-around ml-8">
-                    <li className="navItem hover:shadow-lg flex items-center">
+            <nav className="flex items-center justify-between">
+                <ul className="flex items-stretch justify-around ml-8">
+                    <li className="hover:shadow-lg flex items-center">
                         <Link to='/'>
                             <img src={logo} alt="logo" className="h-14" />
                         </Link>
                     </li>
-                    <li className="navItem hover:shadow-lg flex items-center">
+                    <li className="hover:shadow-lg flex items-center">
                         <Link to='/about' className="p-6">About</Link>
                     </li>
-                    <li className={auth ? "navItem flex items-center hover:shadow-lg" : "navItem hidden items-center hover:shadow-lg"}>
+                    <li className={auth ? "flex items-center hover:shadow-lg" : "hidden items-center hover:shadow-lg"}>
                         <Link to='/tasks' className="p-6">Tasks</Link>
                     </li>
-                    <li className={auth ? "navItem hover:shadow-lg hidden items-center" : "navItem hover:shadow-lg flex items-center"}>
+                    <li className={auth ? "hover:shadow-lg hidden items-center" : "hover:shadow-lg flex items-center"}>
                         <Link to='/login' className="p-6">Login</Link>
                     </li>
-                    <li className={auth ? "navItem hover:shadow-lg hidden items-center" : "navItem hover:shadow-lg flex items-center"}>
+                    <li className={auth ? "hover:shadow-lg hidden items-center" : "hover:shadow-lg flex items-center"}>
                         <Link to='/register' className="p-6">Signup</Link>
                     </li>
                 </ul>
